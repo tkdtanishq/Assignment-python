7b6be9557c3f2d7651e50574384d3d8600f25c18
tkdtanishq <tanishq12122007@gmail.com>
2025-12-20 19:19:56 +0530

    Python-string concatenation

---
+++ Task2.py
@@ -0,0 +1,10 @@
+# Create a Personalized Greeting
+#WAP that:
+#   a.Takes a user's first name and last name as input.
+#   b.Concatenates the first name and last name into a full name.
+#   c.Prints a personalized greeting message using the full name.
+
+f=input("Enter Your First Name:")
+l=input("Enter Your Last Name:")
+fl=f+' '+l+'!'
+print("Hello,",fl," Welcome to the Python Program :D")
