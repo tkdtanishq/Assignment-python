a2a7fd6acbfd062a8780e01bd3ee1d60d1000871
Tanishq Kumar Das <tanishq12122007@gmail.com>
2026-01-03 20:21:26 +0530

    accidental file

--- pract.py
+++
@@ -1,6 +0,0 @@
-#to find if a number is positiive or negative
-a=int(input("Enter the number:"))
-if a>0:
-    print(f"{a} is a positive Number.")
-elif  a==0:
-    print(f"{a} is either 0 or a negative Number.")
