7348832a4bc3ef6c937094e515a66b99feeec9cb
tkdtanishq <tanishq12122007@gmail.com>
2026-01-03 20:08:39 +0530

    Even or Odd

---
+++ a2_task1.py
@@ -0,0 +1,10 @@
+# Check if a Number is Even or Odd.
+# WAP that:
+# 1. Takes an integer input from the user.
+# 2. Checks whether the number is even or odd using if an if - else statement.
+# 3. Display the result accordingly.
+n=int(input("Enter a Number:"))
+if n%2==0:
+    print(f"{n} is a Even Number")
+else:
+    print(f"{n} is a Odd Number")
